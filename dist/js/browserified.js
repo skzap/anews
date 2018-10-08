8a1ff83dcde56b7dc273b7fb635272f730c3d6ea
Adrien M <adrien@d.tube>
2018-10-08 15:30:54 +0200

    add comments in listings

--- dist/js/browserified.js
+++ dist/js/browserified.js
@@ -22930,6 +22930,19 @@ window.bind = {
 template.defaults.imports.percent = function(float) {
     return Math.round(10000*float)/100
 }
+template.defaults.imports.length = function(array) {
+    if (!array || !array.length)
+        return 0;
+    return array.length
+}
+template.defaults.imports.isPlural = function(array) {
+    if (!array || !array.length)
+        return false
+    if (array.length > 1)
+        return true
+    else
+        return false
+}
 var templates = []
 
 proxy.user = null
